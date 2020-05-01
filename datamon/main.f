af85ce2761bb4cb2ab16c04ddf6a635c590d9eba
Rick VanNorman <rick.vannorman@stratolaunch.com>
2020-05-01 09:57:06 -0500

    initial

---
+++ datamon/main.f
@@ -0,0 +1,14 @@
+{ ----------------------------------------------------------------------
+a data display window for arbitrary text
+
+rick vannorman  26apr2020  rick@neverslow.com
+---------------------------------------------------------------------- }
+
+requires rnd
+include example-output
+include gui
+
+datamon builds gui
+
+: go  gui construct ;
+
