a39192db7e80e1837bc355ad67ab7657f9b9b62d
Rick VanNorman <rick.vannorman@stratolaunch.com>
2020-05-07 09:24:47 -0500

    improved format string search

--- toolwin/formatted-output.f
+++ toolwin/formatted-output.f
@@ -9,7 +9,7 @@ to insert an output. the only significant things are that each
 pattern is unique, and that it reserves enough space in the template
 for the desired output. the formatted text will be
 written into the tempate from the beginning of the matched string,
-and will OVERWRITE any text necessary following. 
+and will OVERWRITE any text necessary following.
 
 The example following will show how to use these functions.
 
@@ -18,7 +18,7 @@ Rick VanNorman   6May2020  rick@neverslow.com
 
 { ----------------------------------------------------------------------
 a template builder. the item insertion and format indicators
-MUST be unique. any string will do. 
+MUST be unique. any string will do.
 ---------------------------------------------------------------------- }
 
 : add-string ( addr len -- )
@@ -37,16 +37,23 @@ ITEM    is any forth executable function
 FORMAT  is any forth function to act on the result of the ITEM
         and produce a string (addr len) to be inserted into
         the output string
-        
+
          display-item  <pattern>  <item>   <formatter>
 In use:  display-item  %1         @time    (time)
 
+SEARCH-ERASE searches for the substring (if it exists), erases it,
+and returns its offset in the original string and a flag
 ---------------------------------------------------------------------- }
 
+: search-erase ( haystack len needle len -- offset flag )
+   fourth >r  dup >r   search if
+      drop dup r> blank  r> -  true exit
+   then 2r> 2drop drop false ;
+
 : display-item ( template  <parameters> -- template )
-   dup zcount  tuck bl word count search if
-      ' , ' ,  nip - ,  
-   else 2drop drop  then ;
+   dup zcount  bl word count search-erase if
+      ( xt item) ' ,   ( xt formatter) ' ,  ( offset) ,
+   else drop then ;
 
 { ----------------------------------------------------------------------
 FORMAT-OUTPUT makes a copy of the desired output template in
@@ -66,7 +73,14 @@ puts the result back at PAD in the current context
       swap cmove             \ place it
       r>
    repeat drop  buf @+ pad xplace  pad @+ ;
-   
+
+: measure-output ( ref -- x y )
+   0 0 rot  zcount begin
+      dup 0>
+   while
+      2dup eol-scanner swap >r  /string
+      2swap  1+ swap r> max  swap  2swap
+   repeat 2drop ;
 \ ----------------------------------------------------------------------
 1 [if]
 
@@ -75,7 +89,7 @@ puts the result back at PAD in the current context
 
 : (8u.r)   ( n -- addr len )   8 (u.r) ;
 
-create sample-template   
+create sample-template
 
 here
    +| the sample output template showing fomatted data
@@ -84,19 +98,19 @@ here
    +| data is the last item on the line. if normal text
    +| follows the formatted data it is optional, as
    +| it is on all other lines
-   +| 
-   +|   aaa =  %x_____  is the value of aaa
-   +|   bbb =  %02xxxx  the bbb value
-   +|  time =  %aa      date =  !!........|
-   +| 
-   +| the date field "reserved" more      
+   +|
+   +|   aaa =  %xxxxxxxxx  is the value of aaa
+   +|   bbb =  %02xxxx     the bbb value
+   +|  time =zz%aa      date =  !!........|
+   +|
+   +| the date field "reserved" more
    +| horizontal space than needed with dots
    +| and note that the lines don't have to be of
    +| equal length; the trailing bar just allows
    +| you to guarantee enough room for your data
-0 ,  
+0 ,
 
-   display-item %x      aaa                 (8u.r)
+   display-item %xxxxxxxxx  aaa                 (8u.r)
    display-item %02     bbb                 (8u.r)
    display-item %aa     @time               (time)
    display-item !!      @date               (date)
@@ -105,6 +119,6 @@ drop 0 ,
 
 : showme ( -- )
    sample-template format-output  cr cr types cr cr ;
-   
+
 [then]
 
