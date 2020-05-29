44662466ead2c5c0b883074d812a26338108103e
Rick VanNorman <rick.vannorman@stratolaunch.com>
2020-05-29 12:35:35 -0500

    initial

---
+++ sfx/mimic.f
@@ -0,0 +1,19 @@
+
+oop +order
+: mimic
+   create 0 ,  immediate
+   does> ( addr) .s
+      bl peek-word count s" mimics" compare(nc) 0= if
+         bl word drop  '  >body swap ! exit
+      then ( else) @ ['] >data (object) ;
+previous
+
+class point  single x single y : dot x . y . ; end-class
+class joint  : dot ." up in smoke" ; end-class
+
+point builds pt
+joint builds doobie
+
+mimic foo
+
+
