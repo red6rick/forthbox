06fc57ec20dbffc2c227406106df2641d8db3013
Rick VanNorman <rick.vannorman@stratolaunch.com>
2020-06-01 11:09:49 -0500

    added the mimic function here

--- sfx/oopext.f
+++ sfx/oopext.f
@@ -338,3 +338,26 @@ supreme reopen
 end-class  supreme relink-children
 
 
+{ ----------------------------------------------------------------------
+class point  single x single y : dot x . y . ; end-class
+class joint  : dot ." up in smoke" ; end-class
+
+point builds pt
+joint builds doobie
+
+mimic foo
+
+foo mimics point   foo dot
+foo mimics joint   foo dot
+
+---------------------------------------------------------------------- }
+
+oop +order
+: mimic
+   create 0 ,  immediate
+   does> ( addr) 
+      bl peek-word count s" mimics" compare(nc) 0= if
+         bl word drop  '  >body swap ! exit
+      then ( else) @ ['] >data (object) ;
+previous
+
